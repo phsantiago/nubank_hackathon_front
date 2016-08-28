8d4c4ec46f8eec4d6de94018209b78794ba7bae6
Pedro H. Santiago <pedroh_s_@hotmail.com>
2016-08-28 07:06:46 -0300

    dinheiro

--- src/js/components/Conquistas/Conquistas.js
+++ src/js/components/Conquistas/Conquistas.js
@@ -2,10 +2,13 @@ import React from 'react'
 import { Row, Col, Table, CardTitle, Card, Collection, CollectionItem } from 'react-materialize'
 import Style from './Conquistas.scss'
 
-const conquista = ({TituloConquista, DescricaoConquista, Completada})=>(
+const conquista = ({TituloConquista, DescricaoConquista, Completada, ValorDesconto})=>(
 	<Col s={3}>
 		<Card className="tac archiev">
-			<h5>{TituloConquista}</h5>
+			<h5 className="light-green-text">
+				<b>R$ {ValorDesconto}</b>
+			</h5>
+			<h4>{TituloConquista}</h4>
 				<img className={!Completada ? 'disabled medal' : 'medal'} src="http://i.imgur.com/BnNo0Tk.png" />
 			
 			<p>{DescricaoConquista}</p>
@@ -19,32 +22,38 @@ class Conquistas extends React.Component {
 			{
 		     "TituloConquista": "Ta Saindao da Jaula",
 		     "DescricaoConquista": "Vender sua primeira ação",
-		     "Completada": true
+		     "Completada": true,
+		     "ValorDesconto": 5
 		   },
 		   {
 		     "TituloConquista": "Aquecendo os Motores",
 		     "DescricaoConquista": "Vender sua primeira ação",
-		     "Completada": false
+		     "Completada": false,
+		     "ValorDesconto": 5
 		   },
 		   {
 		     "TituloConquista": "É mais de 300 Reais",
 		     "DescricaoConquista": "Comprar mais de 300$ em ações",
-		     "Completada": false
+		     "Completada": false,
+		     "ValorDesconto": 5
 		   },
 		   {
 		     "TituloConquista": "Amor para Dar e Vender",
 		     "DescricaoConquista": "Vender mais de 300$ em ações",
-		     "Completada": false
+		     "Completada": false,
+		     "ValorDesconto": 5
 		   },
 		   {
 		     "TituloConquista": "Hipster",
 		     "DescricaoConquista": "Compre ações de uma empresa que possua valor no mercado inferior a 500.000$",
-		     "Completada": false
+		     "Completada": false,
+		     "ValorDesconto": 5
 		   },
 		   {
 		     "TituloConquista": "Conservador",
 		     "DescricaoConquista": "Compre ações de uma empresa que possua valor no mercado superior a 100.000.000$",
-		     "Completada": false
+		     "Completada": false,
+		     "ValorDesconto": 5
 		   }
 		]
 		return (
