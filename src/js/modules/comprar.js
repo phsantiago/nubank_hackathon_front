3ce46d8b891b1545a56770712a3feb3cd248f872
Felipe Blassioli <felipeblassioli@gmail.com>
2016-08-28 03:41:47 -0300

    comprar

--- src/js/modules/comprar.js
+++ src/js/modules/comprar.js
@@ -4,7 +4,20 @@ var $ = require('jquery')
 export const BASE_URL = "http://simuladoracoes.hackathon.com.br"
 export const FETCH_EMPRESAS = "FETCH_EMPRESAS"
 export const FETCH_EMPRESAS_RESULT = "FETCH_EMPRESAS_RESULT"
+export const FETCH_NOTICIAS = "FETCH_NOTICIAS"
+export const FETCH_NOTICIAS_RESULT = "FETCH_NOTICIAS_RESULT"
 export const SELECT_STOCK_OPTION = "SELECT_STOCK_OPTION"
+
+export const fetchNoticias = function(idEmpresa) {
+  return (dispatch) => {
+    dispatch({ type: FETCH_NOTICIAS, payload: idEmpresa })
+    $.getJSON(BASE_URL + "/Noticias/Listar/" + idEmpresa)
+      .done(response => {
+        console.log("GOT RESPONSE", response)
+        dispatch({ type: FETCH_NOTICIAS_RESULT, payload: response.ReturnObject })
+    })
+  }
+}
 
 export const fetchEmpresas = function() {
   return (dispatch) => {
@@ -19,31 +32,30 @@ export const fetchEmpresas = function() {
 
 export const selectStockOption = (option) => {
   console.log("OPTION", option)
-  return {
-    type: SELECT_STOCK_OPTION,
-    payload: option
+  return (dispatch) => {
+    dispatch({
+      type: SELECT_STOCK_OPTION,
+      payload: option
+    })
+    dispatch(fetchNoticias(option.IdEmpresa))
   }
 }
 
 const DEFAULT_STATE = Immutable.fromJS({
-  selectedOption: {},
-  visibleStockOptions: [
-{ details: { Abertura: "19.13", MaiorValorDia: "12.32", MenorValorDia: "12.123", MaiorMeses: "123.2", MenorMeses: "123.12", ValorDeAbertura: "12" } }
-    ],
-  newsData: [
-    { img_url: null, title: "O CEO DO MACDONALD MORREU", content: "LOREM IPSUM", source_url: "http://google.com" },
-    { img_url: null, title: "O CEO DO MACDONALD MORREU", content: "LOREM IPSUM", source_url: "http://google.com" },
-    { img_url: null, title: "O CEO DO MACDONALD MORREU", content: "LOREM IPSUM", source_url: "http://google.com" },
-    { img_url: null, title: "O CEO DO MACDONALD MORREU", content: "LOREM IPSUM", source_url: "http://google.com" },
-    { img_url: null, title: "O CEO DO MACDONALD MORREU", content: "LOREM IPSUM", source_url: "http://google.com" },
-    { img_url: null, title: "O CEO DO MACDONALD MORREU", content: "LOREM IPSUM", source_url: "http://google.com" },
-  ]
+  selectedOption: null,
+  visibleStockOptions: [],
+  newsData: []
 })
 
 const comprarReducer = (state = DEFAULT_STATE, action) => {
   if(state)
     console.log("STATE", state.toJS())
   switch(action.type) {
+    case FETCH_NOTICIAS:
+      return state
+    case FETCH_NOTICIAS_RESULT:
+      console.log("QUERY RESULT", action.payload)
+      return state.set("newsData", action.payload)
     case FETCH_EMPRESAS:
       return state
     case FETCH_EMPRESAS_RESULT:
