91572b6ee3ad77545f93753ca94af49b4e854ab1
Pedro H. Santiago <pedroh_s_@hotmail.com>
2016-08-27 23:59:52 -0300

    vender e hosts

--- src/js/components/Vender/Vender.js
+++ src/js/components/Vender/Vender.js
@@ -1,13 +1,56 @@
 import React from 'react'
+import Styles from './Vender.scss'
+import { Row, Col, Button, Card, Collection, CollectionItem } from 'react-materialize'
+
+const ListItem = ({img, name, qt, val}) => (
+	<CollectionItem>
+		<Row className="valign-wrapper">
+			<Col s={1}>
+				<img src={img} className="circle responsive-img" />
+			</Col>
+			<Col s={7}>
+				{name}
+			</Col>
+			<Col s={2}>
+				{qt + "x" + val}
+			</Col>
+			<Col s={2}>
+				<Button waves='light'>
+					Vender
+				</Button>
+			</Col>
+		</Row>
+	</CollectionItem>
+)
+class ActionList extends React.Component {
+  render() {
+    const {history} = this.props
+
+    return (
+      <Collection>
+      	{history.map(ListItem)}
+      </Collection>
+    )
+  }
+}
 
 class Vender extends React.Component {
   render() {
+  	var array = [
+  		{img:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Mcdonalds-90s-logo.svg/2000px-Mcdonalds-90s-logo.svg.png", name:"McDonald's", qt:"2", val:"123R$"},
+  		{img:"https://diariodopresal.files.wordpress.com/2010/09/petrobras-br.jpg", name:"Petrobras", qt:"4", val:"100R$"},
+  	]
     return (
-        <div>
-          <h1>Vender</h1>
-        </div>
+    	<Row>
+	        <Col s={9}>
+	        	<Card>
+	          		<ActionList history={array} />
+	        	</Card>
+	        </Col>
+    	</Row>
     )
   }
 }
 
 export default Vender
+
