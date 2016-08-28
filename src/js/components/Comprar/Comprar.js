6558ea8fe708a5a62edc5bb4f3c2e0f492d83945
Felipe Blassioli <felipeblassioli@gmail.com>
2016-08-28 06:56:56 -0300

    ShowBuyModal

--- src/js/components/Comprar/Comprar.js
+++ src/js/components/Comprar/Comprar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Row, Col, Table, CardTitle, Card, Collection, CollectionItem } from 'react-materialize'
+import { Modal, Button, Row, Col, Table, CardTitle, Card, Collection, CollectionItem } from 'react-materialize'
 import News from '../../containers/News'
 import Style from './Comprar.scss'
 
@@ -38,19 +38,108 @@ const Details = ({ ValorDeAbertura, MaiorValorDia, MenorValorDia, MaiorMeses, Me
   </Card>
 )
 
+class UnitChooser extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { value: this.props.initialValue || 0 }
+  }
+
+  increment() {
+    let newValue = this.state.value + 1,
+        _onChange = this.props.onChange
+    this.setState({ value: newValue }, () => { _onChange(newValue) })
+  }
+
+  decrement() {
+    let newValue = this.state.value - 1,
+        _onChange = this.props.onChange
+    if(newValue < 0)
+      newValue = 0
+    this.setState({ value: newValue }, () => { _onChange(newValue) })
+  }
+
+  render() {
+    let props = this.props,
+        inc = this.increment.bind(this),
+        dec = this.decrement.bind(this)
+
+    let _style = {}
+    if(this.state.value > this.props.limit)
+      _style = { color: "red" }
+    return (<div {...props} style={{ display: "inline" }}>
+			<button className="btn-purple"
+        onClick={dec}>
+				-
+			</button>
+
+      <span style={_style}>{ this.state.value }</span>
+			<button className="btn-purple"
+        onClick={inc}>
+				+
+			</button>
+    </div>)
+  }
+}
+
+class ShowBuyModal extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { buyCount: 0 }
+  }
+
+  render() {
+    let props = this.props
+    let { option, onBuyClick, balance } = props
+    let { NomeEmpresa, CotacaoRecente } = option
+
+    let totalCost = this.state.buyCount * option.CotacaoRecente,
+        _onChange = (v) => { this.setState({ buyCount: v }) }
+    return (
+        <Modal
+          header={"Quero comprar acoes da " + NomeEmpresa +"!"}
+          actions={[ <Button>Comprar!</Button> ]}
+          trigger={
+            <Button className="pink accent-4" waves='light'>
+              Comprar
+            </Button>
+          }>
+          <div>
+            <p>Seu saldo disponivel eh R$ { balance }</p>
+            <p>
+            No momento, uma acao de <strong>{ NomeEmpresa}</strong> custa R$ <strong>{ CotacaoRecente }</strong>.</p>
+
+            <p>Eu gostaria de comprar <UnitChooser limit={balance} onChange={_onChange} inline={true} /> acoes num total de <strong>R$ {totalCost} </strong>.</p>
+
+          </div>
+        </Modal>
+    )
+  }
+}
+
 const BuyActionsItem = (props) => {
-    const { NomeEmpresa, UrlLogo, CotacaoRecente, Variacao, onItemClick } = props
+    let { NomeEmpresa,
+          UrlLogo,
+          CotacaoRecente,
+          Variacao,
+          buyStock,
+          onItemClick } = props
     return (
       <tr onClick={() => { onItemClick(props) }} {...props}>
         <td><img src={UrlLogo} className="logo-xs" /></td>
         <td>{ NomeEmpresa }</td>
         <td>{ CotacaoRecente }</td>
         <td>{ Variacao }</td>
+        <td>
+          <ShowBuyModal
+            balance={props.balance || 0}
+            onBuyClick={buyStock}
+            option={props} />
+        </td>
       </tr>
     )
 }
 
-const BuyActions = ({ onItemClick, stockOptions }) => (
+const BuyActions = ({ onItemClick, buyStock, stockOptions }) => (
     <Card title="Quais ações eu posso comprar?">
       <Table bordered={true} className="tbl-emp">
         <thead>
@@ -59,11 +148,12 @@ const BuyActions = ({ onItemClick, stockOptions }) => (
             <th>Empresa</th>
             <th>Cotacao</th>
             <th>VariacaoRecente</th>
+            <th></th>
           </tr>
         </thead>
 
         <tbody>
-          { stockOptions.map((o) => ({ ...o, onItemClick })).map(BuyActionsItem) }
+          { stockOptions.map((o) => ({ ...o, buyStock, onItemClick })).map(BuyActionsItem) }
         </tbody>
       </Table>
     </Card>
@@ -89,6 +179,7 @@ class Comprar extends React.Component {
 
           <Col s={4}>
             <BuyActions
+              buyStock={(x) => { console.log("BUY", x) }}
               onItemClick={selectStockOption}
               stockOptions={visibleStockOptions} />
           </Col>
